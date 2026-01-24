947299a1a668c2363a4dae0caec5fa65967317a4
Andre-tech671 <andre@example.com>
2026-01-24 04:46:25 +0200

    Setting Up working environment

--- src/token_frontend/src/main.jsx
+++ src/token_frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from "./components/App";
+import '../public/main.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const init = async () => { 
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+init();
+
